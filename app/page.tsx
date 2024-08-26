b5c0ac5c2197817ba99d7e2d51b8de1aa0281b4c
Max Korsunov <maxkors2014@gmail.com>
2024-08-26 18:43:31 +0300

    feat: use new UI package

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 'use client';
 import { useConnect, useWalletManifests } from '@/app/hooks';
 import { useInfo } from '@/app/fetchers';
+import { PenumbraUIProvider } from '@penumbra-zone/ui/PenumbraUIProvider';
+import { Density } from '@penumbra-zone/ui/Density';
+import { Button } from '@penumbra-zone/ui/Button';
+import { Text } from '@penumbra-zone/ui/Text';
+import { WalletBalance } from '@penumbra-zone/ui/WalletBalance';
+import { AddressViewComponent } from '@penumbra-zone/ui/AddressViewComponent';
 
 export default function Home() {
   const { data: wallets, loading } = useWalletManifests();
@@ -8,54 +14,57 @@ export default function Home() {
   const { address, balances } = useInfo(connected);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {loading && <p>Wallets loading...</p>}
+    <PenumbraUIProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {loading && <Text body>Wallets loading...</Text>}
 
-      {!loading && !connected && (
-        <ul className='flex flex-col gap-2'>
-          {!Object.keys(wallets).length && (
-            <p>
-              No injected wallets found. Try{' '}
-              <a className='underline' href='https://chromewebstore.google.com/detail/prax-wallet/lkpmkhpnhknhmibgnmmhdhgdilepfghe' target='_blank'>
-                installing Prax
-              </a>
-            </p>
-          )}
+        {!loading && !connected && (
+          <ul className='flex flex-col gap-2'>
+            {!Object.keys(wallets).length && (
+              <Text body>
+                No injected wallets found. Try{' '}
+                <a className='underline'
+                   href='https://chromewebstore.google.com/detail/prax-wallet/lkpmkhpnhknhmibgnmmhdhgdilepfghe'
+                   target='_blank'>
+                  installing Prax
+                </a>
+              </Text>
+            )}
 
-          {Object.entries(wallets).map(([origin, manifest]) => (
-            <button
-              type="button"
-              key={origin}
-              disabled={connectionLoading}
-              className='border rounded cursor-pointer border-gray-300 px-4 py-2 hover:bg-gray-800 transition'
-              onClick={() => onConnect(origin)}
-            >
-              {connectionLoading ? 'Connecting...' : `Connect to ${manifest.name}`}
-            </button>
-          ))}
-        </ul>
-      )}
-
-      {connected && (
-        <section>
-          <h3 className='text-lg font-bold'>Connected!</h3>
-          <button
-            type="button"
-            className='mt-2 mb-4 border rounded cursor-pointer border-gray-300 px-4 py-2 hover:bg-gray-800 transition'
-            onClick={onDisconnect}
-          >
-            Disconnect
-          </button>
-
-          <p className='mb-4 break-all'>Your address is {address}</p>
-          <p>Balances</p>
-          <ul>
-            {balances.map((balance, index) => (
-              <li key={index}>{balance}</li>
+            {Object.entries(wallets).map(([origin, manifest]) => (
+              <Button
+                key={origin}
+                onClick={() => onConnect(origin)}
+                disabled={connectionLoading}
+              >
+                {connectionLoading ? 'Connecting...' : `Connect to ${manifest.name}`}
+              </Button>
             ))}
           </ul>
-        </section>
-      )}
-    </main>
+        )}
+
+        {connected && (
+          <section className='flex flex-col gap-2'>
+            <Text h3>Connected!</Text>
+            <Density compact>
+              <Button actionType='destructive' onClick={onDisconnect}>Disconnect</Button>
+            </Density>
+
+            <Text body>
+              Your address is
+              <AddressViewComponent addressView={address} />
+            </Text>
+            <Text h4>
+              Balances
+            </Text>
+            <ul>
+              {balances.map((balance, index) => (
+                <WalletBalance key={index} balance={balance} />
+              ))}
+            </ul>
+          </section>
+        )}
+      </main>
+    </PenumbraUIProvider>
   );
 }
